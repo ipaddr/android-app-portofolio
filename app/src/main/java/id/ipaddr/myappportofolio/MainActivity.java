0565b3d9a97bc7ddb2e2ba8f60d480ff6086aa4f
Ulfia Izzati <ulfiaizzati@Ulfias-MacBook-Air.local>
2016-10-12 10:17:02 +0800

    Add style and button

--- app/src/main/java/id/ipaddr/myappportofolio/MainActivity.java
+++ app/src/main/java/id/ipaddr/myappportofolio/MainActivity.java
@@ -2,6 +2,9 @@ package id.ipaddr.myappportofolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method will handle all button click action within activity_main layout
+     * @param view is button that has been clicked
+     */
+    public void onButtonClicked(View view){
+        String text = ((Button)view).getText().toString();
+        Toast.makeText(this, String.format(getString(R.string.button_click_message), text), Toast.LENGTH_SHORT).show();
     }
 }
